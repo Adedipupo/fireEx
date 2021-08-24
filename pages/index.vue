06f9946766bb53a2151b5062830f4711bf21a39e
Benedict <jokanolaadedipupo@gmail.com>
2021-08-24 18:32:32 +0100

    hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,12 @@
 <template>
+ <div class="container">
   <Hero />
+ </div>
 </template>
 
 <script>
-import Hero from '../components/Hero.vue'
 
 export default {
-  components: {
-    Hero
-  }
+
 }
 </script>
