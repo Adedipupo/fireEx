69dbb04d45d85096a81920a8f97f56f415f08520
Benedict <jokanolaadedipupo@gmail.com>
2021-08-25 14:29:19 +0100

    get product by id

--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-        <h1>id </h1>
+        <h1>{{products}}} </h1>
     </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
     export default {
-        
+        computed:{
+            ...mapState([
+                'products'
+            ])
+        }
     }
 </script>
 
