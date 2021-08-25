9c184190ed05867a1d5796948768bb6cb9b1bd25
Benedict <jokanolaadedipupo@gmail.com>
2021-08-25 02:54:16 +0100

    navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,30 @@
 <template>
-  <div class="container">
-    <Hero />
-    <LargeCardDisplay
-      v-for="cardInfo in largeCardInfo"
-      :key="cardInfo.id"
-      :cardsSection="cardInfo"
-    />
-    <SmallCardDisplay
-     v-for="smallCardInfo in smallCardInfo"
-     :key="smallCardInfo.id"
-     :smallCardsSection="smallCardInfo"
-     />
+  <div>
+    <Nav />
+    <div class="container">
+      <Hero />
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="smallCardInfo in smallCardInfo"
+        :key="smallCardInfo.id"
+        :smallCardsSection="smallCardInfo"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import { largeCardSections } from "@/assets/data.js";
-import { smallCardSections } from "@/assets/data.js";
+import { largeCardSections, smallCardSections } from "@/assets/data.js";
 
 export default {
   data() {
     return {
       largeCardInfo: largeCardSections,
-      smallCardInfo: smallCardSections,
+      smallCardInfo: smallCardSections
     };
   }
 };
