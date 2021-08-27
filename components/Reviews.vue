9f3f830fac213aad5f7704e2e391898f5b07c86c
Benedict <jokanolaadedipupo@gmail.com>
2021-08-27 07:52:44 +0100

    fetching reviewres data

--- components/Reviews.vue
+++ components/Reviews.vue
@@ -1,15 +1,25 @@
 <template>
+  <div>
+    <h3>Customer Reviews</h3>
     <div>
-        <h3>Customer Reviews</h3>
+      {{ reviewers }}
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-         
-    }
+export default {
+  data() {
+    return {
+      reviewers: []
+    };
+  },
+  async fetch() {
+    this.reviewers = await fetch(
+      "https://randomuser.me/api/?results=5"
+    ).then(res => res.json());
+  }
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
