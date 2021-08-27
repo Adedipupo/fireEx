dee51519da345191df9d412185ed057556a67171
Benedict <jokanolaadedipupo@gmail.com>
2021-08-27 07:23:35 +0100

    error:page not found

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="container">
-    <h1>My Items</h1>
+    <div>
+
     </div>
 </template>
 
 <script>
     export default {
-        layout: "no-nav"
+        
     }
 </script>
 
-<style  scoped>
+<style scoped>
 
 </style>
